ee7a35018f508f385354073391c668e6102fcf26
Indrakant Mishra <indrakantm23@gmail.com>
2021-09-04 18:50:13 +0530

    initial commit

---
+++ src/shared/header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <header>
+                
+            </header>
+        )
+    }
+}
+
+export default Header;
