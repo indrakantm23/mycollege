3bbcec5ba88595b5c57d1f82fd5cfe3dbc943f9e
Indrakant Mishra <indrakantm23@gmail.com>
2021-09-04 18:44:40 +0530

    initial project

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className=''>
+          <h1>My College</h1>  
+      </div>
+    )
+  }
+}
+
+export default App;
